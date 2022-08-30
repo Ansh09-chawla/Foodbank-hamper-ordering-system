cde3d1e98f42603d7855173c4aaf1a477198013b
Ansh09-chawla <73417447+Ansh09-chawla@users.noreply.github.com>
2022-08-30 11:58:13 -0600

    Add files via upload

---
+++ edu/ucalgary/ensf409/OrderSummary.java
@@ -0,0 +1,15 @@
+/**
+@author Aleksander Rudolf, Ansh Singh, Jaskaran Bhatia
+@version 2.0
+@since 1.0 - Mar. 28/2022
+*/
+
+package edu.ucalgary.ensf409;
+
+public interface OrderSummary {
+    /**
+     * This method is implemented inside Order class.
+     * @return
+     */
+	String formatSummary();
+}
